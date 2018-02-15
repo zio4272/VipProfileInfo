07a7a144fc8d3977294f3a54f5fc9fbc1c5e652b
zio <zio4272@naver.com>
2018-02-15 21:39:18 +0900

    마사지를부탁해 아이콘 / splash 변경

--- app/src/main/java/com/cgb/vipprofileinfo/BoardActivity.java
+++ app/src/main/java/com/cgb/vipprofileinfo/BoardActivity.java
@@ -179,7 +179,7 @@ public class BoardActivity extends BaseActivity {
                 startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
             }
         });
-        webview.loadUrl("http://zio.raonnet.com/");
+        webview.loadUrl("http://dododo.raonnet.com/");
     }
 
     public class WebAppInterface {
